a87d94e10b1d1605ecabd2760fcd202bb1798beb
AndreCA1 <carvalhoandre149@gmail.com>
2025-01-26 22:57:07 -0300

    Criação da classe formularios onde agrupei as funções de leitura de dados(cliente, vendedor, endereço e produtos) e retirada dos codigos inline dessas funções na classe Autenticação

--- ecommerce/src/ecommerce/view/Autenticacao.java
+++ ecommerce/src/ecommerce/view/Autenticacao.java
@@ -1,158 +1,28 @@
 package ecommerce.view;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-
 //Controller
 import ecommerce.controller.EcommerceController;
 
-//views
-import ecommerce.view.Menus;
-
-//Scanner
-import java.util.Scanner;
-
 //Entitys
 import ecommerce.model.entity.Cliente;
-import ecommerce.model.entity.Endereco;
 import ecommerce.model.entity.Vendedor;
 
 public class Autenticacao {
-    
     Menus menu = new Menus();
-    
-    public Endereco cadastroEndereco() {
-        Endereco endereco = null;
-        
-        System.out.println("**************************");
-        System.out.println("** Cadastro de Endereço **");
-        System.out.println("**************************");
-        
-        try (Scanner input = new Scanner(System.in)) {
-            endereco = new Endereco();
-            
-            System.out.print("Cep: \t");
-            endereco.setCep(input.nextInt());
-            input.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
-
-            System.out.print("\nRua: \t");
-            endereco.setRua(input.nextLine());
-
-            System.out.print("\nComplemento: \t");
-            endereco.setComplemento(input.nextLine());
-
-            System.out.print("\nLogradouro: \t");
-            endereco.setLogradouro(input.nextLine());
-
-            System.out.print("\nBairro: \t");
-            endereco.setBairro(input.nextLine());
-
-            System.out.print("\nCidade: \t");
-            endereco.setCidade(input.nextLine());
-
-            System.out.print("\nEstado: \t");
-            endereco.setEstado(input.nextLine());
-
-            System.out.print("\nNúmero: \t");
-            endereco.setNumero(input.nextInt());
-        }
-        
-        return endereco;
-    }
-    
-    public static java.sql.Date parseToSqlDate(String dateStr) {
-        try {
-            // Usando SimpleDateFormat para converter a String em java.util.Date
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            java.util.Date utilDate = sdf.parse(dateStr);
-
-            // Convertendo para java.sql.Date
-            return new java.sql.Date(utilDate.getTime());
-        } catch (Exception e) {
-            System.out.println("Erro ao converter a data: " + e.getMessage());
-            return null; // Retorna null em caso de erro
-        }
-    }
+    Formulario formulario = new Formulario();
     
     public void cadastroCliente() {
-        Cliente cliente = new Cliente();
-        
-        System.out.println("**************************");
-        System.out.println("** Cadastro de Cliente **");
-        System.out.println("**************************");
-        
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Digite o nome: \t");
-        cliente.setNome(input.nextLine());
-
-        System.out.print("\nDigite o CPF: \t");
-        cliente.setCpf(input.nextLine());
-
-        System.out.print("\nDigite o e-mail: \t");
-        cliente.setEmail(input.nextLine());
-
-        System.out.print("\nDigite a senha: \t");
-        cliente.setSenha(input.nextLine());
-
-        System.out.print("\nDigite o telefone: \t");
-        cliente.setTelefone(input.nextLine());
-
-        // Coleta de data de nascimento (Como Date, é necessário um formato específico)
-        System.out.print("\nDigite a data de nascimento (dd/MM/yyyy): \t");
-        String dataNascimentoStr = input.nextLine();
-        cliente.setDataNascimento(parseToSqlDate(dataNascimentoStr));
-
-        System.out.print("\nDigite a nacionalidade: \t");
-        cliente.setNacionalidade(input.nextLine());
-
-        System.out.print("\nDigite o gênero: \t");
-        cliente.setGenero(input.nextLine());
-        }
+        Cliente cliente = formulario.lerCliente();
         
         //set endereco
-        cliente.setEndereco(cadastroEndereco());
+        cliente.setEndereco(formulario.lerEndereco());
     }
     
     public void cadastroVendedor() {
-        Cliente cliente = new Cliente();
-        
-        System.out.println("**************************");
-        System.out.println("** Cadastro de Vendedor **");
-        System.out.println("**************************");
-        
-        try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Digite o nome: \t");
-            cliente.setNome(input.nextLine());
+        Vendedor vendedor = formulario.lerVendedor();
 
-            System.out.print("\nDigite o CPF: \t");
-            cliente.setCpf(input.nextLine());
-
-            System.out.print("\nDigite o CNPJ: \t");
-            cliente.setCpf(input.nextLine());
-
-            System.out.print("\nDigite o e-mail: \t");
-            cliente.setEmail(input.nextLine());
-
-            System.out.print("\nDigite a senha: \t");
-            cliente.setSenha(input.nextLine());
-
-            System.out.print("\nDigite o telefone: \t");
-            cliente.setTelefone(input.nextLine());
-
-            // Coleta de data de nascimento (Como Date, é necessário um formato específico)
-            System.out.print("\nDigite a data de nascimento (dd/MM/yyyy): \t");
-            String dataNascimentoStr = input.nextLine();
-            cliente.setDataNascimento(parseToSqlDate(dataNascimentoStr));
-
-            System.out.print("\nDigite a nacionalidade: \t");
-            cliente.setNacionalidade(input.nextLine());
-
-            System.out.print("\nDigite o gênero: \t");
-            cliente.setGenero(input.nextLine());
-        }
-        
         //set endereco
-        cliente.setEndereco(cadastroEndereco());
+        vendedor.setEndereco(formulario.lerEndereco());
     }
     
     public int login(String email, String senha) {
---
+++ ecommerce/src/ecommerce/view/Formulario.java
@@ -0,0 +1,168 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecommerce.view;
+import ecommerce.model.entity.Cliente;
+import ecommerce.model.entity.Endereco;
+import java.util.Scanner;
+
+import ecommerce.model.entity.Produto;
+import ecommerce.model.entity.Vendedor;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ * @author Andre
+ */
+public class Formulario {
+    
+    public Produto lerProduto(int id){
+        Scanner scan = new Scanner(System.in);
+        Produto produto = new Produto();
+        Vendedor vendedor = new Vendedor();
+        
+        System.out.print("Digite o nome do produto: ");
+        produto.setNome(scan.nextLine());
+        System.out.print("Digite a descrição do produto: ");
+        produto.setDescricao(scan.nextLine());
+        System.out.print("Digite o valor do produto: ");
+        produto.setValor(scan.nextDouble());
+        System.out.print("Digite a quantidade em estoque: ");
+        produto.setEstoque(scan.nextInt());
+        
+        vendedor.setId(id);
+        produto.setVendedor(vendedor);
+        return produto;
+    }
+    
+    public Endereco lerEndereco() {
+        Endereco endereco = null;
+        
+        System.out.println("**************************");
+        System.out.println("** Cadastro de Endereço **");
+        System.out.println("**************************");
+        
+        try (Scanner input = new Scanner(System.in)) {
+            endereco = new Endereco();
+            
+            System.out.print("Cep: \t");
+            endereco.setCep(input.nextInt());
+            input.nextLine(); // Consumir a quebra de linha deixada pelo nextInt()
+
+            System.out.print("\nRua: \t");
+            endereco.setRua(input.nextLine());
+
+            System.out.print("\nComplemento: \t");
+            endereco.setComplemento(input.nextLine());
+
+            System.out.print("\nLogradouro: \t");
+            endereco.setLogradouro(input.nextLine());
+
+            System.out.print("\nBairro: \t");
+            endereco.setBairro(input.nextLine());
+
+            System.out.print("\nCidade: \t");
+            endereco.setCidade(input.nextLine());
+
+            System.out.print("\nEstado: \t");
+            endereco.setEstado(input.nextLine());
+
+            System.out.print("\nNúmero: \t");
+            endereco.setNumero(input.nextInt());
+        }
+        
+        return endereco;
+    }
+    
+    public Vendedor lerVendedor(){
+        Scanner input = new Scanner(System.in);
+        Vendedor vendedor = new Vendedor();
+        
+        System.out.println("**************************");
+        System.out.println("** Cadastro de Vendedor **");
+        System.out.println("**************************");
+        
+        System.out.print("Digite o nome: \t");
+        vendedor.setNome(input.nextLine());
+
+        System.out.print("\nDigite o CPF: \t");
+        vendedor.setCpf(input.nextLine());
+
+        System.out.print("\nDigite o CNPJ: \t");
+        vendedor.setCnpj(input.nextLine());
+
+        System.out.print("\nDigite o e-mail: \t");
+        vendedor.setEmail(input.nextLine());
+
+        System.out.print("\nDigite a senha: \t");
+        vendedor.setSenha(input.nextLine());
+
+        System.out.print("\nDigite o telefone: \t");
+        vendedor.setTelefone(input.nextLine());
+
+        // Coleta de data de nascimento (Como Date, é necessário um formato específico)
+        System.out.print("\nDigite a data de nascimento (dd/MM/yyyy): \t");
+        String dataNascimentoStr = input.nextLine();
+        vendedor.setDataNascimento(parseToSqlDate(dataNascimentoStr));
+
+        System.out.print("\nDigite a nacionalidade: \t");
+        vendedor.setNacionalidade(input.nextLine());
+
+        System.out.print("\nDigite o gênero: \t");
+        vendedor.setGenero(input.nextLine());
+        
+        return vendedor;
+    }
+    
+    public Cliente lerCliente(){
+        Cliente cliente = new Cliente();
+        
+        System.out.println("**************************");
+        System.out.println("** Cadastro de Cliente **");
+        System.out.println("**************************");
+        
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Digite o nome: \t");
+            cliente.setNome(input.nextLine());
+
+            System.out.print("\nDigite o CPF: \t");
+            cliente.setCpf(input.nextLine());
+
+            System.out.print("\nDigite o e-mail: \t");
+            cliente.setEmail(input.nextLine());
+
+            System.out.print("\nDigite a senha: \t");
+            cliente.setSenha(input.nextLine());
+
+            System.out.print("\nDigite o telefone: \t");
+            cliente.setTelefone(input.nextLine());
+
+            // Coleta de data de nascimento (Como Date, é necessário um formato específico)
+            System.out.print("\nDigite a data de nascimento (dd/MM/yyyy): \t");
+            String dataNascimentoStr = input.nextLine();
+            cliente.setDataNascimento(parseToSqlDate(dataNascimentoStr));
+
+            System.out.print("\nDigite a nacionalidade: \t");
+            cliente.setNacionalidade(input.nextLine());
+
+            System.out.print("\nDigite o gênero: \t");
+            cliente.setGenero(input.nextLine());
+        }
+        return cliente;
+    }
+    
+    public static java.sql.Date parseToSqlDate(String dateStr) {
+        try {
+            // Usando SimpleDateFormat para converter a String em java.util.Date
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            java.util.Date utilDate = sdf.parse(dateStr);
+
+            // Convertendo para java.sql.Date
+            return new java.sql.Date(utilDate.getTime());
+        } catch (Exception e) {
+            System.out.println("Erro ao converter a data: " + e.getMessage());
+            return null; // Retorna null em caso de erro
+        }
+    }
+}
