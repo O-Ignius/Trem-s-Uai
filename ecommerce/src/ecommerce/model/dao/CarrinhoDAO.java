24995011d5f39bef2e7c4e003ac147f36734728d
AndreCA1 <carvalhoandre149@gmail.com>
2025-01-26 23:06:22 -0300

    Criação da função buscaCarrinhoAtual

--- ecommerce/src/ecommerce/model/dao/CarrinhoDAO.java
+++ ecommerce/src/ecommerce/model/dao/CarrinhoDAO.java
@@ -7,8 +7,11 @@ package ecommerce.model.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 import ecommerce.model.entity.Cliente;
+import ecommerce.model.entity.Carrinho;
+
 /**
  *
  * @author Andre
@@ -22,14 +25,37 @@ public class CarrinhoDAO {
 
     // Método para salvar um novo Carrinho
     public void salvar(Cliente cliente) {
-        String sql = "INSERT INTO carrinho (cliente_id) VALUES (?)";
+        String sql = "INSERT INTO carrinho (cliente_id, fechado) VALUES (?, ?)";
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setInt(1, cliente.getId());
+            stmt.setInt(2, 0);
             stmt.execute();
             stmt.close();
         } catch (SQLException u) {
             throw new RuntimeException(u);
         }
+    }
+    
+    public Carrinho buscaCarrinhoAtual(int id_Cliente){
+        String sql = "select c.id from carrinho c where c.cliente_id =" + id_Cliente + "and c.fechado = 0;";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            if(rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setId(id_Cliente);
+                
+                Carrinho carrinho = new Carrinho();
+                carrinho.setId(rs.getInt("id"));
+                carrinho.setFechado(false);
+                carrinho.setCliente(cliente);
+                
+                return carrinho;
+            }
+        } catch (SQLException u) {
+            throw new RuntimeException(u);
+        }
+        return null;
     }
 }
